699f876c1a30fa656d314d7be89f300dd5d7aaa4
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-03-31 11:20:06 -0400

    refactored code to operate on self

--- my_amazing_array.rb
+++ my_amazing_array.rb
@@ -1,26 +1,29 @@
 #puts all the code the pry developers have written and gives us access
 require "pry"
+#require 'my_amazing_test'
 
 class Array
 
 #changing x to self will set it to handle any array its called on.
 
-	def bubble_sort (x)
+	def bubble_sort
 
-	  for num in 1..x.length
+	  for num in 1..self.length
 	  i = 0
-		  while i < (x.length-1)
-		    if x[i] > x[i+1]
-		      x[i], x[i+1] = x[i+1],x[i] 
+		  while i < (self.length-1)
+		    if self[i] > self[i+1]
+		      self[i], self[i+1] = self[i+1],self[i] 
 		    end 
 		    i += 1    
 		  end
 	  end
-	  print x
+	  print self
 	end
 end
 
-Array.new.bubble_sort([1,4,3])
+#Array.new.bubble_sort([1,4,3])
+
+#[1,3,2,0].bubble_sort
 
 #[1,3,2].bubble_sort
 
