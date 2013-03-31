ec43a113f0d176cc364c417dffb601f15bb67c0c
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-03-31 10:46:44 -0400

    adding bubble_sort under class Array

--- my_amazing_array.rb
+++ my_amazing_array.rb
@@ -1,19 +1,30 @@
-# class Array
-#   # Examine each pair in myself and swap them until all my
-#   # elements are in order
-#   #
-def bubble_sort(x)
-  for num in 1..x.length
-  i = 0
-  while i < (x.length-1)
-    if x[i] > x[i+1]#print "#{[x[i], x[i+1]]}"
-      x[i], x[i+1] = x[i+1],x[i]
-    
-    end 
-    i += 1    
-  end
+#puts all the code the pry developers have written and gives us access
+require "pry"
+
+class Array
+
+#changing x to self will set it to handle any array its called on.
+
+	def bubble_sort (x)
+
+	  for num in 1..x.length
+	  i = 0
+		  while i < (x.length-1)
+		    if x[i] > x[i+1]
+		      x[i], x[i+1] = x[i+1],x[i] 
+		    end 
+		    i += 1    
+		  end
+	  end
+	  print x
+	end
 end
 
-#     self
-#   end
-# end
+Array.new.bubble_sort([1,4,3])
+
+#[1,3,2].bubble_sort
+
+#array = [2,4,3,9,7]
+#bubble_sort(array)
+#array.bubble_sort
+# print array
